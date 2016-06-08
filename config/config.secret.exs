aaa287be4a27eced95eae4792d2d30fb59f73519
= <steiner.mischa@gmail.com>
2016-06-08 17:23:36 +0900

    temporarily push the config.secret file to heroku

---
+++ config/config.secret.exs
@@ -0,0 +1,5 @@
+use Mix.Config
+
+config :tradenara,
+  mailgun_domain: "https://api.mailgun.net/v3/sandbox0a844c80df3f4455aeab98ed590668ea.mailgun.org",
+  mailgun_key: "key-43545adbdd68995b7c6e0e454ee26193"
