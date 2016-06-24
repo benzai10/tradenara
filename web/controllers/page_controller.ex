1bcbd61b256b36fdae2c85f0176a1ae6b17c5e77
= <steiner.mischa@gmail.com>
2016-06-24 16:40:06 +0900

    updated visuals

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule Tradenara.PageController do
   use Tradenara.Web, :controller
+  alias Tradenara.Category
+  alias Tradenara.Company
+  alias Tradenara.Article
 
   def index(conn, _params) do
-    render conn, "index.html"
+    categories = Repo.all(Category)
+    companies = Repo.all(Company)
+    articles = Repo.all(Article)
+    render conn, "index.html", categories: categories, companies: companies, articles: articles
   end
 end
