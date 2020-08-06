24dbb8325e781b3808c4e2417f0f39f4670fc927
benzai10 <mischa@benzai10.com>
2020-08-06 13:48:58 +0200

    obfuscate mailgun key

--- config/config.secret.exs
+++ config/config.secret.exs
@@ -2,4 +2,4 @@ use Mix.Config
 
 config :tradenara,
   mailgun_domain: "https://api.mailgun.net/v3/sandbox0a844c80df3f4455aeab98ed590668ea.mailgun.org",
-  mailgun_key: "key-43545adbdd68995b7c6e0e454ee26193"
+  mailgun_key: MAILGUN_KEY
